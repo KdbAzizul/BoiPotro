b3c8213a746c7fff5de5ec8e0a5dd03491b6b176
KdbAzizul <kdbazizul@gmail.com>
2025-06-03 15:00:26 +0600

    ProductScreen Completed

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -10,12 +10,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
 import HomeScreen from './screens/HomeScreen.jsx';
+import ProductScreen from './screens/ProductScreen.jsx';
 
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<HomeScreen/>}/>
+      <Route path="/product/:id" element={<ProductScreen/>}/>
     </Route>
+    
   )
 )
 
