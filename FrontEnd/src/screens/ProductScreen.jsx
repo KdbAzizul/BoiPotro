6d76c3402167b68b1537153e2cd472d656316e88
KdbAzizul <kdbazizul@gmail.com>
2025-06-04 16:02:58 +0600

    fetched data

--- FrontEnd/src/screens/ProductScreen.jsx
+++ FrontEnd/src/screens/ProductScreen.jsx
@@ -1,13 +1,23 @@
 import {useParams} from "react-router-dom"
-import products from "../products"
+import {useEffect,useState} from 'react'
+import axios from "axios";
 import { Link } from "react-router-dom";
 import {Row,Col,Image,ListGroup,Card,Button, ListGroupItem} from 'react-bootstrap'
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
+    const [product,setProduct]=useState({});
+
+    
 
     const {id:productId} = useParams();
-    const product=products.find((p)=>p._id.toString()===productId)
+    useEffect(() => {
+      const fetchProduct= async () => {
+        const {data} = await axios.get(`/api/products/${productId}`);
+        setProduct(data);
+      }
+      fetchProduct();
+    },[productId]);
     
   return (
     <>
