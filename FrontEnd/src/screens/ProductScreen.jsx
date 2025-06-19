c0716d65147dd5c4df08e350924216d9c27d8b54
KdbAzizul <kdbazizul@gmail.com>
2025-06-19 20:53:46 +0600

    cart Utils done

--- FrontEnd/src/screens/ProductScreen.jsx
+++ FrontEnd/src/screens/ProductScreen.jsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 //import {useEffect,useState} from 'react'
 //import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Row,
@@ -10,21 +11,35 @@ import {
   Card,
   Button,
   ListGroupItem,
+  Form,
 } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
 
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const [qty,setQty]=useState(1);
+
+ 
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
 
+  const addToCartHandler=()=>{
+    dispatch(addToCart({...product,qty}));
+    navigate('/cart');
+  }
   // const [product,setProduct]=useState({});
   // useEffect(() => {
   //   const fetchProduct= async () => {
@@ -93,12 +108,33 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                
+                {product.stock>0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Quantity</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}>
+                          {[...Array(product.stock).keys()].map((x) =>(
+                            <option key={x +1} value={x+1}>
+                              {x+1}
+                            </option>
+                          ) )}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
 
                 <ListGroup.Item>
                   <Button
                     className="btn-block"
                     type="button"
                     disabled={product.Stock === 0}
+                    onClick={addToCartHandler}
                   >
                     Add To Cart
                   </Button>
---
+++ FrontEnd/src/slices/cartSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../Utils/cartUtils";
+
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems:[]};
+
+
+
+const cartSlice=createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addToCart:(state,action)=>{
+            const item=action.payload;
+
+            const existItem=state.cartItems.find((x)=>x.id===item.id);
+
+            if(existItem){
+                state.cartItems=state.cartItems.map((x)=>x.id===existItem.id ? item:x )
+            }else{
+                state.cartItems=[...state.cartItems,item];
+            }
+            return updateCart(state);
+            
+        },
+        
+        
+        
+    }
+})
+
+export const {addToCart}=cartSlice.actions;
+export default cartSlice.reducer;
